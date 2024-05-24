90ded33679202449c1afb2307f73740832379661
skunichetty <skunich@umich.edu>
2024-05-24 17:25:08 -0400

    Switched to Next.JS

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>skunichetty.dev</h1>
+    </div>
+  );
+}
--- src/_pages/index.md
+++
@@ -1,5 +0,0 @@
----
-layout: home.njk
-title: skunichetty.dev
-permalink: "index.html"
----
