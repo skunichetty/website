451acee3705b3f5a3e0c12364b11778f27fef594
skunichetty <skunich@umich.edu>
2024-05-25 21:16:12 -0400

    Added animations!

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,27 @@
+"use client";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
+
 export default function Home() {
-  return <div></div>;
+  const main = useRef(null);
+
+  useGSAP(
+    () => {
+      gsap.from(".line1", { y: 25, opacity: 0, duration: 1 });
+      gsap.from(".line2", { y: 35, opacity: 0, duration: 1.15 });
+    },
+    { scope: main }
+  );
+
+  return (
+    <div>
+      <div className="block h-fit mt-6" ref={main}>
+        <div className="line1 text-4xl">Hi, I&apos;m Sachchit.</div>
+        <div className="line2 text-2xl mt-1">
+          I&apos;m a software engineer and computer scientist.
+        </div>
+      </div>
+    </div>
+  );
 }
